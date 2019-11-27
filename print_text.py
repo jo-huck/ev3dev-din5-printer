14b533f49ee47631c3d2f9e8dd18b620b12b9d80
Jonas Hucklenbroich <johuck@linux.fritz.box>
2019-11-27 17:44:28 +0100

    penMove function now works properly

--- print_text.py
+++ print_text.py
@@ -20,12 +20,14 @@ btn = Button()
 
 cl.mode='COL-COLOR'
 
-print("Text: %s" % (sys.argv[1]))
+# print("Text: %s" % (sys.argv[1]))
 colors=('unknown','black','blue','green','yellow','red','white','brown')
 
 def penMove(x,y,penDown):
     global penPositionX
     global penStateDown
+    xSpeed = defaultSpeed
+    ySpeed = defaultSpeed
     if penDown == True and penDown != penStateDown:
         print("pen goes down")
         penLift.on_for_degrees(speed=defaultSpeed, degrees=-200)
@@ -34,15 +36,24 @@ def penMove(x,y,penDown):
         print("pen goes up")
         penLift.on_for_degrees(speed=defaultSpeed,degrees=200)
         penStateDown = False
+
     if x != 0:
+        if (y>x and x != 0 and y/x != 0):
+            # xSpeed = (defaultSpeed / (y/x)) * 2
+            xSpeed = defaultSpeed * (x/y)
+            print("--> Calculated xSpeed: ", xSpeed)
         print("pen moves ", x)
-        penRL.on_for_degrees(speed=defaultSpeed,degrees=x)
+        penRL.on_for_degrees(speed=xSpeed,degrees=x,block=(y == 0))
         penPositionX += x
         print("pen position is now: ", penPositionX)
     if y != 0:
-        print("pen moves ", y)
-        feeder.on_for_degrees(speed=defaultSpeed, degrees=y)
-        
+        if (x>y and y != 0 and x/y != 0):
+            ySpeed = defaultSpeed * (y/x)
+            print("--> Calculated ySpeed: %s", ySpeed)
+        print("pen moves ", -(y/2))
+        feeder.on_for_degrees(speed=ySpeed, degrees=-y)
+
+
 
 def feedIn():
     print("feeding paper in")
@@ -55,17 +66,18 @@ def feedIn():
 
 def feedOut():
     print("feeding paper out")
-    penMove(500,0,False)
-    sleep(5)
-    penLift.on_for_degrees(speed=defaultSpeed, degrees=800)
+    penMove(500-penPositionX,0,False)
+    sleep(0.5)
+    penLift.on_for_degrees(speed=defaultSpeed, degrees=700)
     feeder.on_for_degrees(speed=defaultSpeed*2, degrees=500)
     print("Paper feeding finished")
 
-
+# x = R/L
+# y = F/B
 # def print_1():
 feedIn()
-sleep(5)
-penMove(0,0,True)
-sleep(5)
+sleep(2)
+penMove(x=500,y=500,penDown=True)
+sleep(2)
 
 feedOut()
